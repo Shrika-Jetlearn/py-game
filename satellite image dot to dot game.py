8145ee46651ccdd52d7139fdb04434c966252e3b
Shrika-Jetlearn <shrikab@icloud.com>
2025-09-20 12:36:19 +0100

    Add files via upload

--- satellite image dot to dot game.py
+++ satellite image dot to dot game.py
@@ -1,5 +1,10 @@
 import pgzrun
 import random
+from time import time 
+
+start_time=0
+total_time=0
+end_time=0
 
 WIDTH=500
 HEIGHT=500
@@ -12,7 +17,10 @@ for i in range(total):
     satellite.x=random.randint(50,450)
     satellite.y=random.randint(50,450)
     sats.append(satellite)
+start_time=time()
+
 def draw():
+    global total_time
     screen.blit("space background", (0,0))
     num=1
     for satellite in sats:
@@ -21,6 +29,12 @@ def draw():
       num+=1 
     for line in lines:
        screen.draw.line(line[0],line[1], (255,255,255))
+    if next_satellite < total:
+       total_time = time()-start_time 
+       total_time=round(total_time,2)
+       screen.draw.text(str(total_time),(10,10))
+    else:
+       screen.draw.text(str(total_time),(10,10))
 def on_mouse_down(pos):
    global next_satellite,lines
    if next_satellite < total:
